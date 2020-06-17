226d68b1ae1ec9641388c156e5d3a9a262dfeaf5
Lincest <474617032@qq.com>
2020-06-18 07:50:38 +0800

    update

---
+++ 3.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int lengthOfLongestSubstring(string s) {
+	int ans = 0;
+	int cnt = 0;
+	unordered_map<char, int> mp;
+	int r = 0;
+	int l = 0;
+	while (r < s.size()) {
+		if (!mp.count(s[r]) || mp[s[r]] < l) {
+			mp[s[r]] = r;
+			cnt++;
+		}
+		else {
+			cnt = r - mp[s[r]];
+			l = mp[s[r]] + 1;
+			mp[s[r]] = r; 
+		}
+		ans = max(ans, cnt);
+		r++;
+	}
+	return ans;
+}
+
+
+
+
+int main() {
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+
+	string s;
+	cin >> s;
+	cout << lengthOfLongestSubstring(s) << endl;
+
+	return 0;
+}
